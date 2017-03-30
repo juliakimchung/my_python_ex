e56cd778caaeb12c54a139e11ac738ccc481579b
juliakimchung <julhkm@gmail.com>
2017-03-30 10:07:24 -0500

    exercise classes

--- classes_objects.py
+++ classes_objects.py
@@ -35,26 +35,33 @@ anna.marks.append(64)
 print(anna.average())
 print(anna.marks)
 
-# class Store:
-#     def __init__(self, name):
-#         self.name = name
-#         self.items = []
-#         # You'll need 'name' as an argument to this method.
-#         # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+        # You'll need 'name' as an argument to this method.
+        # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
     
-#     def add_item(self, name, price):
-#         item = {"name":name, "price": price}
-#         return self.items.append(item)
-#         # Create a dictionary with keys name and price, and append that to self.items.
+    def add_item(self, name, price):
+        item = {"name":name, "price": price}
+        self.items.append(item)
+        return self.items
+        # Create a dictionary with keys name and price, and append that to self.items.
 
-#     def stock_price(self):
-        
-#             return sum(self.items['price'])
-#         # Add together all item prices in self.items and return the total.
+    def stock_price(self):
+        total = 0
+        for item in self.items:
+            total += item['price']
+        return total
+            
+        # Add together all item prices in self.items and return the total.
 
-# store = Store("pottery barn")
-# print(store.add_item('bedset', '1999.00'))
-# # print(store.stock_price())
+store = Store("pottery barn")
+print(store.add_item('tableset', 2200.00))
+print(store.add_item('bedset', 1999.00))
+print(store.add_item('drawerset', 3000.00))
+print(store.items)
+print(store.stock_price())
 
 
 
